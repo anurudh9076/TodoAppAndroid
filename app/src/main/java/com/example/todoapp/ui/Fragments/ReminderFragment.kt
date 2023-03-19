ce47a541d4b5f1bbe9401520c9c7bf10d28723e9
anurudh9076 <anurudh9076@gmail.com>
2023-03-20 00:13:06 +0530

    hola

--- app/src/main/java/com/example/todoapp/ui/Fragments/ReminderFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/Fragments/ReminderFragment.kt
@@ -21,7 +21,7 @@ class ReminderFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+hello
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
